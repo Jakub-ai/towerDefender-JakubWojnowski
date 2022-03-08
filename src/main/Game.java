093ecbe49de60ab10135b26355494178f5aa187a
Jakub-ai <83953649+Jakub-ai@users.noreply.github.com>
2022-03-08 13:41:09 +0100

    dzien 2
    dodałem atlas ktory mam w tablicy i wyswietlam randomowo postacie

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,16 +1,38 @@
 package main;
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.awt.image.BufferedImage;
 
 public class Game extends JFrame{
 private GameScreen gameScreen;
+
+private BufferedImage img;
+
+
     public Game(){
+
+      importImg();
+
         setSize(640, 640);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        gameScreen = new GameScreen();
+        gameScreen = new GameScreen(img);
         add(gameScreen);
     }
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("atlas.png");
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args){
         System.out.println("start Game!!!");
         Game game = new Game();
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -1,25 +1,48 @@
 package main;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JPanel;
 public class GameScreen extends JPanel {
     private Random random;
-    public GameScreen(){
+    private BufferedImage img;
+    private ArrayList<BufferedImage> atlas = new ArrayList<>();
+    public GameScreen(BufferedImage img){
+        this.img = img;
+        LoadAtlas();
     random = new Random();
+
     }
+
+    private void LoadAtlas() {
+        for (int h = 0;  h < 3; h++){
+            for(int w = 0; w < 10; w++ ){
+                atlas.add( img.getSubimage(w * 32, h * 32, 32, 32));
+
+            }
+        }
+    }
+
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
+       // g.drawImage(atlas.get(27),0,0,null);
+   /* BufferedImage z = img.getSubimage(32*0,32,32,32);
+    g.drawImage(z,0,0,null);*/
 
 
-        for(int y = 0; y < 20; y++){
+      for(int y = 0; y < 20; y++){
        for(int x = 0; x < 20; x++){
-          g.setColor (getRndColor());
-           g.fillRect(x * 32, y * 32, 32,32);
+           g.drawImage(atlas.get(getRandInt()),x*32, y*32, null );
+
        }
 
         }
 
+    }
+    private int getRandInt(){
+        return random.nextInt(30);
     }
     private Color getRndColor(){
         int r = random.nextInt(255);
